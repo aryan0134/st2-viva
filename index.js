78deb13e57babc12c354903af77151b6cc6a015b
aryan0134 <aryan0134.be20@chitkara.edu.in>
2022-08-16 11:34:04 +0530

    final

---
+++ index.js
@@ -0,0 +1,13 @@
+var navList=document.getElementById("nav-bar");
+var navIcon=document.getElementById("nav-icon");
+navList.style.maxHeight="0px";
+function toggleMenu(){
+    if(navList.style.maxHeight=="0px"){
+        navList.style.maxHeight="260px";
+    }
+    else{
+        navList.style.maxHeight="0px";
+    }
+}
+
+navIcon.addEventListener("click",toggleMenu);
